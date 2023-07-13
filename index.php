6f76b8c3842722ef9e9fd0d1caae1c0e7ca525ed
jonathanjeremi <jonathanjeremisiagian@gmail.com>
2023-07-14 00:02:44 +0700

    Adding index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jasuke</title>
+</head>
+<body>
+    <h1>Penjualan Jasuke</h1>
+    <p>Diobral dengan harga murmer</p>
+</body>
+</html>
